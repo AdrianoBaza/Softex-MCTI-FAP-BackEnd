e74a5ad5bed4e4156ca3318939dbcfaac2db4389
AdrianoBaza <bazafordev@gmail.com>
2023-09-07 16:04:18 -0300

    Cadastro Pessoa - variáveis - declaracao - atribuição

---
+++ Exercitando/cadastroPessoa.js
@@ -0,0 +1,21 @@
+/*
+Criar um cadastro de pessoas com os seguintes dados: nome, salário, idade, se possui diploma. Identificar os tipos de cada variável. Desenvolver o código utilizando exemplo para cada variável que respeitem os tipos identificados. Certificar que os valores correspondam aos tipos de dados corretos:
+*/
+
+// Declaração das variáveis
+let nome;
+let idade;
+let salario;
+let possuiDiploma;
+
+// Atribuição de valor as variáveis
+nome = "Adriano"; // string
+idade = 49; // number
+salario = 3580; // number
+possuiDiploma = true; // boolean
+
+console.log('Impressão para certificar o tipo de dado de cada variável')
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof salario);
+console.log(typeof possuiDiploma);
