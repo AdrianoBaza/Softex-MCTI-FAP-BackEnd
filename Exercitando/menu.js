9ad926e37ce5000c840b15cda3d4eae6618cecee
AdrianoBaza <bazafordev@gmail.com>
2023-09-11 13:53:54 -0300

    Menu estático: testa captura do teclado e print no console

---
+++ Exercitando/menu.js
@@ -0,0 +1,14 @@
+/*
+console.log('======== MENU ========');
+console.log('1 - Listar Clientes');
+console.log('2 - Cadastrar Cliente');
+console.log('3 - Editar Cliente');
+console.log('4 - Remover Cliente');
+console.log('0 - Sair do Sistema');
+console.log('=======================');
+console.log('Opção: ____')
+console.log('=======================');
+
+const input = require('readline-sync');
+let opcao = input.question('Informe a sua opção: ');
+*/
