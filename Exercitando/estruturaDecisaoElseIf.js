a088105f9e73b5e7adeb71d544b4181c7203fe5b
AdrianoBaza <bazafordev@gmail.com>
2023-09-12 08:37:43 -0300

    Exercicios - condicional else if; checagem positivo ou negativo, par ou ímpar, divisibilidade, tipo de dado

---
+++ Exercitando/estruturaDecisaoElseIf.js
@@ -0,0 +1,90 @@
+/*
+Exercícios:
+*/
+
+
+// Classifique um número em três categorias: negativo, zero ou positivo.
+
+let numero = 0;
+
+if (numero < 0) {
+    console.log(`${numero} é um número negativo`)
+} else if (numero == 0 || numero == '') {
+    console.log(`${numero} é igual a zero`);
+} else {
+    console.log(`${numero} é um número positivo`)
+}
+
+
+/*
+// Verifique se um número é divisível por 2, 3 ou 5.
+
+let numero = 17;
+
+if (numero % 2 == 0) {
+    console.log(`${numero} é divisível por 2`);
+} else if (numero % 3 == 0) {
+    console.log(`${numero} é divisível por 3`);
+} else if (numero % 5 == 0) {
+    console.log(`${numero} é divisível por 5`);
+} else {
+    console.log(`${numero} não é divisível por 2, 3 nem 5`)
+}
+*/
+
+
+/*
+// Classifique uma variável em quatro categorias: booleana, numérica, string ou indefinida.
+
+let aprovado = false;
+console.log(`conteúdo da variável: ${aprovado}`);
+
+if (typeof aprovado == 'boolean') {
+    console.log(`Categoria: ${typeof aprovado}`);
+} else if (typeof aprovado == 'number') {
+    console.log(`Categoria: ${typeof aprovado}`);
+} else if (typeof aprovado == 'string') {
+    console.log(`Categoria: ${typeof aprovado}`);
+} else  if (typeof aprovado == 'undefined') {
+    console.log(`Categoria: ${typeof aprovado}`);
+} else {
+    console.log(`A variável aprovado não é do tipo boolean, numérica, string nem indefinida`);
+}
+*/
+
+
+/*
+// Classifique um número em cinco categorias: negativo, zero, positivo, par ou ímpar.
+
+let numero = 0;
+console.log(`Numero informado: ${numero}`);
+
+if (numero < 0) {
+    console.log(`é negativo`);
+} else if (numero == 0) {
+    console.log(`é igual a ${numero}`);
+} else if (numero > 0) {
+    console.log(`é positivo`);
+} 
+
+if (numero % 2 == 0) {
+    console.log(`é par`);
+} else {
+    console.log(`é ímpar`);
+} 
+*/
+
+/*
+// Verifique se um número é divisível por 2, 3, 5 ou 7.
+let numero = 721;
+
+if (numero % 2 == 0) {
+    console.log(`${numero} é divisível por 2`);
+} else if (numero % 3 == 0) {
+    console.log(`${numero} é divisível por 3`);
+} else if (numero % 5 == 0) {
+    console.log(`${numero} é divisível por 5`);
+} else  if (numero % 7 == 0)  {
+    console.log(`${numero} é divisível por 7`);
+}
+*/
